c2dfe2b26eced9bd90bfa7679744f43e5cdd7d46
Michael Sivolobov <astronomer@gmail.com>
2016-05-12 20:57:53 +0600

    Created IpGeoBase class

---
+++ src/Geocoder/Provider/IpGeoBase.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Mikhail A. Sivolobov <astronomer@gmail.com>
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider;
+
+
+/**
+ * Class that provides IpGeoBase service as Geocoder Provider
+ */
+class IpGeoBase
+{
+
+}
