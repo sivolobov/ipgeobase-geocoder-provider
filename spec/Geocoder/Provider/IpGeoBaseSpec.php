2d11eed727671f8f7d9763880865db30cfd4813a
Michael Sivolobov <astronomer@gmail.com>
2016-05-12 21:35:01 +0600

    Added `geocode()` implementation

--- spec/Geocoder/Provider/IpGeoBaseSpec.php
+++ spec/Geocoder/Provider/IpGeoBaseSpec.php
@@ -11,4 +11,22 @@ class IpGeoBaseSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Geocoder\Provider\IpGeoBase');
     }
+
+    function it_should_implement_geocoder_provider_interface()
+    {
+        $this->shouldImplement('Geocoder\Provider\Provider');
+    }
+    
+    function it_retrieves_address_collection_by_ip()
+    {
+        $this->geocode('91.221.60.82')->shouldHaveType('Geocoder\Model\AddressCollection');
+    }
+    
+    function it_throws_an_exception_while_trying_to_pass_not_an_ip()
+    {
+        $this->shouldThrow('Geocoder\Exception\UnsupportedOperation')->during('geocode', [null]);
+        $this->shouldThrow('Geocoder\Exception\UnsupportedOperation')->during('geocode', ['test']);
+        $this->shouldThrow('Geocoder\Exception\UnsupportedOperation')->during('geocode', ['256.255.255.255']);
+        $this->shouldThrow('Geocoder\Exception\UnsupportedOperation')->during('geocode', ['1.1.1.256']);
+    }
 }
--- src/Geocoder/Provider/IpGeoBase.php
+++ src/Geocoder/Provider/IpGeoBase.php
@@ -7,12 +7,57 @@
  */
 
 namespace Geocoder\Provider;
+use Geocoder\Exception\UnsupportedOperation;
+use Geocoder\Model\AddressCollection;
 
 
 /**
  * Class that provides IpGeoBase service as Geocoder Provider
  */
-class IpGeoBase
+class IpGeoBase implements Provider
 {
+    /**
+     * @inheritDoc
+     */
+    public function geocode($address)
+    {
+        if (!filter_var($address, FILTER_VALIDATE_IP)) {
+            throw new UnsupportedOperation('The FreeGeoIp provider does not support street addresses.');
+        }
+
+        return new AddressCollection();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function reverse($latitude, $longitude)
+    {
+        // TODO: Implement reverse() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLimit()
+    {
+        // TODO: Implement getLimit() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function limit($limit)
+    {
+        // TODO: Implement limit() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        // TODO: Implement getName() method.
+    }
 
 }
